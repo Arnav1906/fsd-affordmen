e7523a2119eb8a4a5a694a488bddd802ef77eebf
Arnav Bhalla <bhalla.arnav19@gmail.com>
2025-06-10 09:17:54 +0000

    updated deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
-import './App.css';
-import Home from './components/home/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/pages/Login';
-import Register from './components/pages/Register';
-import Dashboard from './components/pages/Dashboard';
+import "./App.css";
+import Home from "./components/home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import Dashboard from "./components/pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <div className="App">
-          <Route path="/" element={<Home />} />
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='*' element={<h1>404 Page Not Found</h1>} />
-          <h1>Hello World!</h1>
-        </div>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
       </Routes>
     </Router>
   );
